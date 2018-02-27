fd890dd10ed54e74b433c55e6f75b4a3bb883fa0
dwqs <461147874@qq.com>
2018-02-28 00:54:07 +0800

    refactor: Basic render

--- examples/app.vue
+++ examples/app.vue
@@ -1,6 +1,8 @@
 <template>
     <div class='v2-list-example'>
-        <v2-lazy-list></v2-lazy-list>
+        <div class="demo-wrap">
+            <v2-lazy-list v-bind:data='list' v-bind:height="520"></v2-lazy-list>
+        </div>
     </div>
 </template>
 
@@ -8,8 +10,32 @@
     export default {
         data () {
             return {
-                
+                list: [
+                    1,2,3,4,5,6,7,8,9,10,
+                    11,12,13,14,15,16,17,18,19,20
+                ]
             }
         }
     }
 </script>
+
+<style lang="less">
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #f4f4f4;
+    }
+    .v2-list-example {
+        margin: 0 auto;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        width: 960px;
+        background-color: #fff;
+    }
+    .demo-wrap {
+        margin: 0 auto;
+        width: 750px;
+        height: 520px;
+        border: 1px solid #d1d5da;
+    }
+</style>
--- src/lazy-list.vue
+++ src/lazy-list.vue
@@ -1,28 +1,112 @@
 <template>
-    <div class='v2-lazy-list-wrap'>
-        v2-lazy-list-wrap
+    <div class='v2-lazy-list-wrap' ref="wrap" v-bind:style="{
+        height: containerHeight + 'px'
+    }">
+        <ul class='v2-lazy-list' ref="content">
+            <li class='lazy-list-item' v-for="(item, index) in data" v-bind:key="index">
+                {{item}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     import './list.less';
 
+    import ScrollBar from './scrollbar/index.js';
+
+    const CONTAINER_MIN_HEIGHT = 320;
+    const ITEM_MIN_HEIGHT = 20;
+
     export default {
         name: 'v2-lazy-list',
         props: {
+            data: {
+                type: Array,
+                default: () => []
+            },
 
+            height: {
+                type: Number,
+                default: CONTAINER_MIN_HEIGHT
+            },
+
+            itemHeight: {
+                type: Number,
+                default: ITEM_MIN_HEIGHT * 2
+            }
         },
 
         data () {
-            return {
+            const ch = Number.parseInt(this.height);
+            const ih = Number.parseInt(this.itemHeight);
 
+            return {
+                renderList: [],
+                isRenderAll: false,
+                scrollTop: 0,
+
+                containerWith: 0,
+                containerHeight: (Number.isNaN(ch) || ch < CONTAINER_MIN_HEIGHT) ? CONTAINER_MIN_HEIGHT : ch,
+                ih: (Number.isNaN(ih) || ih < ITEM_MIN_HEIGHT) ? ITEM_MIN_HEIGHT : ih,
+
+                scrollbar: null
             };
+        },
+
+        watch: {
+            data (val) {
+                this.initRenderList();
+            },
+
+            scrollTop (val) {
+                if (this.isRenderAll) {
+                    return;
+                }
+
+            }
         },
 
         provide () {
             return {
                 list: this
             };
+        },
+
+        methods: {
+            initRenderList () {
+                this.isRenderAll = this.renderList.length === this.data.length;
+                if (this.isRenderAll) {
+                    return;
+                }
+
+                const initCount = Math.ceil(this.containerHeight / this.ih) + 1;
+                this.renderList = initCount < this.data.length ? [].concat(this.data.slice(0, initCount)) : [].concat(this.data);
+            },
+
+            updateRenderList () {
+
+            },
+
+            updateScrollVal ({ scrollLeft, scrollTop }) {
+                console.log('scrollTop', scrollTop);
+                this.scrollTop = scrollTop;
+            }
+        },
+
+        mounted () {
+            this.containerWith = this.$el.clientWidth;
+            // this.containerHeight = this.$el.clientHeight;
+
+            console.log('wwww', this.containerHeight);
+            this.initRenderList();
+            this.$nextTick(() => {
+                this.scrollbar = new ScrollBar(this.$el, {
+                    contentWidth: this.$refs.content.scrollWidth,
+                    contentHeight: Math.ceil(this.data.length * this.ih), //this.$refs.content.scrollHeight,
+                    callBack: this.updateScrollVal
+                })
+            });
         }
     };
 </script>
